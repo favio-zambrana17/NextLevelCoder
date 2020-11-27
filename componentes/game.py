395b7b98ae5c483e0daf00f428ad1820f5d3ac84
favio-zambrana17 <74917362+favio-zambrana17@users.noreply.github.com>
2020-11-27 19:46:50 -0400

    Clase_3
    clase_3

--- componentes/game.py
+++ componentes/game.py
@@ -1,5 +1,7 @@
 import pygame
-from utils.constants import (SCREEN_HEIGHT, SCREEN_WIDTH, TITLE)
+
+from componentes.player import Player
+from utils.constants import (SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, BLACK)
 
 
 class Game:
@@ -8,26 +10,34 @@ class Game:
         pygame.display.set_caption(TITLE)
         #un ejemplo
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
     def run(self):
         self.create_components()
         #gameloop:
         self.playing = True
         while self.playing:
+            self.clock.tick(60)
             self.events()
             self.update()
             self.draw()
         pygame.quit()  #metodo para terminar el juego - metodo
 
     def create_components(self):
-        pass
+        self.all_sprites = pygame.sprite.Group()
+        player = Player()
+        self.all_sprites.add(player)
 
     def update(self):
-        pass
+        self.all_sprites.update()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #QUIT - constante
                 self.playing = False
     def draw(self):
-        pass
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
 
