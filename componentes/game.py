eb89cfe467d16b8e3221a9a33694e78fef6e0135
favio-zambrana17 <74917362+favio-zambrana17@users.noreply.github.com>
2020-12-03 20:10:29 -0400

    Clase_8
    show start screen :)

--- componentes/game.py
+++ componentes/game.py
@@ -1,9 +1,11 @@
 import pygame
+import self as self
 
 from componentes.ball import Ball
 from componentes.player import Player
-from utils.constants import (SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, BLACK)
-
+from os import path
+from utils.text_utils import draw_text
+from utils.constants import (SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, BLACK, IMG_DIR)
 
 
 class Game:
@@ -12,7 +14,11 @@ class Game:
         pygame.display.set_caption(TITLE)
         #un ejemplo
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.background_img = pygame.image.load(path.join(IMG_DIR, "spacefield.png")).convert()
+        self.background_img = pygame.transform.scale(self.background_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.playing = False
+        self.running = True
 
     def run(self):
         self.create_components()
@@ -23,7 +29,6 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        pygame.quit()  #metodo para terminar el juego - metodo
 
     def create_components(self):
         self.all_sprites = pygame.sprite.Group()
@@ -53,13 +58,33 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #QUIT - constante
                 self.playing = False
+                self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                  self.player.shoot()
 
     def draw(self):
-        self.screen.fill(BLACK)
+        background_rect = self.background_img.get_rect()
+        self.screen.blit(self.background_img, background_rect)
         self.all_sprites.draw(self.screen)
         pygame.display.flip()
+
+    def show_start_screen(self):
+        self.screen.blit(self.background_img, self.background_img.get_rect())
+        draw_text(self.screen, "GAME working!!", 64, SCREEN_WIDTH/2, SCREEN_HEIGHT/4)
+        draw_text(self.screen, "presiona las teclas de direccionales y SPACE para disparar", 20, SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        draw_text(self.screen, "Press ENTER key to begin", 20, SCREEN_WIDTH/2, SCREEN_HEIGHT*3/5)
+
+        pygame.display.flip()
+        waiting = True
+        while waiting:
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit(0)
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RETURN:
+                        waiting = False
 
 
--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,5 @@
+from os import path
+
 # SCREEN
 SCREEN_HEIGHT = 480
 SCREEN_WIDTH = 800
@@ -9,3 +11,5 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+IMG_DIR = path.join(path.dirname(__file__), "..","img")
